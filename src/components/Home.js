16241bcad51004a23a9257f0e72f0a742c1e3c53
ezeszuchet <szuchetezequiel@gmail.com>
2021-07-19 09:35:07 -0300

    Loader and product details state

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,23 +2,33 @@ import React, { Fragment, useEffect } from 'react'
 
 import MetaData from './layout/MetaData'
 import Product from './product/Product'
+import Loader from './layout/Loader'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { getProducts } from '../actions/productActions'
+import { useAlert } from 'react-alert';
 
 const Home = () => {
 
+    const alert = useAlert();
     const dispatch = useDispatch();
 
     const { loading, products, error, productCount } = useSelector(state => state.products)
 
     useEffect(() => {
+        
+        if(error) {
+            return alert.error(error)
+        } 
+
         dispatch(getProducts());
-    }, [dispatch])
+        
+
+    }, [dispatch, error])
 
     return (
         <Fragment>
-            {loading ? <h1>Loading...</h1> : (
+            {loading ? <Loader/> : (
                 <Fragment>
                     <MetaData title={'Buy Best Products Online'} />
                     <h1 id="products_heading">Latest Products</h1>
