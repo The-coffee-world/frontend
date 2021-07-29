0ea8822f5462a613703905c659e1d3f29279c83b
ezeszuchet <szuchetezequiel@gmail.com>
2021-07-29 19:50:42 -0300

    Add filter by rating

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,6 +20,7 @@ const Home = ({ match }) => {
     const [currentPage, setCurrentPage] = useState(1)
     const [price, setPrice] = useState([1, 1000])
     const [category, setCategory] = useState('')
+    const [rating, setRating] = useState(0)
 
     const categories = [
         'Electronics',
@@ -49,17 +50,17 @@ const Home = ({ match }) => {
             return alert.error(error)
         }
 
-        dispatch(getProducts(keyword, currentPage, price, category));
+        dispatch(getProducts(keyword, currentPage, price, category, rating));
 
 
-    }, [dispatch, alert, error, keyword, currentPage, price, category])
+    }, [dispatch, alert, error, keyword, currentPage, price, category, rating])
 
     const setCurrentPageNo = pageNumber => {
         setCurrentPage(pageNumber)
     }
 
     let count = productCount;
-    if(keyword) {
+    if (keyword) {
         count = filteredProductsCount
     }
 
@@ -94,7 +95,7 @@ const Home = ({ match }) => {
                                                 value={price}
                                                 onChange={price => setPrice(price)}
                                             />
-                                            <hr className="my-5"/>
+                                            <hr className="my-5" />
 
                                             <div className="mt-5">
                                                 <h4 className="mb-3">
@@ -104,7 +105,7 @@ const Home = ({ match }) => {
                                                 <ul className="pl-0">
                                                     {categories.map(category => (
                                                         <li
-                                                            style={{cursor: 'pointer', listStyleType: 'none'}}
+                                                            style={{ cursor: 'pointer', listStyleType: 'none' }}
                                                             key={category}
                                                             onClick={() => setCategory(category)}
                                                         >
@@ -113,20 +114,45 @@ const Home = ({ match }) => {
                                                     ))}
                                                 </ul>
                                             </div>
-                                            
+
+                                            <hr className="my-3" />
+
+                                            <div className="mt-5">
+                                                <h4 className="mb-3">
+                                                    Ratings
+                                                </h4>
+
+                                                <ul className="pl-0">
+                                                    {[5, 4, 3, 2, 1].map(star => (
+                                                        <li
+                                                            style={{ cursor: 'pointer', listStyleType: 'none' }}
+                                                            key={star}
+                                                            onClick={() => setRating(star)}
+                                                        >
+                                                            <div className="rating-outer">
+                                                                <div className="rating-inner" style={{
+                                                                    width: `${star * 20}%`
+                                                                }}>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+
                                         </div>
                                     </div>
                                     <div className="col-6 col-md-9">
                                         <div className="row">
                                             {products.map(product => (
-                                                <Product key={product._id} product={product} col={4}/>
+                                                <Product key={product._id} product={product} col={4} />
                                             ))}
                                         </div>
                                     </div>
                                 </Fragment>
                             ) : (
                                 products.map(product => (
-                                    <Product key={product._id} product={product} col={3}/>
+                                    <Product key={product._id} product={product} col={3} />
                                 ))
                             )}
 
