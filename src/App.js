3b5a952896cb08fccbf847b7b28beaf7943a8375
ezeszuchet <szuchetezequiel@gmail.com>
2021-09-30 22:10:32 -0300

    Create order details component

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Payment from './components/cart/Payment'
 import OrderSuccess from './components/cart/OrderSuccess'
 
 import ListOrders from './components/order/ListOrders'
+import OrderDetails from './components/order/OrderDetails'
 
 import Login from './components/user/Login'
 import Register from './components/user/Register'
@@ -74,6 +75,7 @@ function App() {
           <ProtectedRoute path="/password/update" component={UpdatePassword} exact />
 
           <ProtectedRoute path="/orders/me" component={ListOrders} exact />
+          <ProtectedRoute path="/order/:id" component={OrderDetails} />
         </div>
         <Footer />
       </div>
