1512830fcc4b242511a414b9c839ba5f78787d74
ezeszuchet <szuchetezequiel@gmail.com>
2021-10-12 03:15:59 -0300

    Product admin done

--- src/App.js
+++ src/App.js
@@ -8,15 +8,18 @@ import Footer from './components/layout/Footer';
 import Home from './components/Home';
 import ProductDetails from './components/product/ProductDetails'
 
+// Cart Imports
 import Cart from './components/cart/Cart'
 import Shipping from './components/cart/Shipping'
 import ConfirmOrder from './components/cart/ConfirmOrder'
 import Payment from './components/cart/Payment'
 import OrderSuccess from './components/cart/OrderSuccess'
 
+// Order Imports
 import ListOrders from './components/order/ListOrders'
 import OrderDetails from './components/order/OrderDetails'
 
+// Auth or User Imports
 import Login from './components/user/Login'
 import Register from './components/user/Register'
 import Profile from './components/user/Profile'
@@ -25,8 +28,15 @@ import UpdatePassword from './components/user/UpdatePassword'
 import ForgotPassword from './components/user/ForgotPassword'
 import NewPassword from './components/user/NewPassword'
 
+// Admin Imports
+import Dashboard from './components/admin/Dashboard'
+import ProductsList from './components/admin/ProductsList';
+import NewProduct from './components/admin/NewProduct';
+import UpdateProduct from './components/admin/UpdateProduct';
+
 import ProtectedRoute from './components/route/ProtectedRoute'
-import { loadUser, updateProfile } from './actions/userActions'
+import { loadUser } from './actions/userActions'
+import { useSelector } from 'react-redux';
 import store from './store'
 import axios from 'axios';
 
@@ -46,6 +56,8 @@ function App() {
 
     getStripeApiKey();
   }, [])
+
+  const { user, loading } = useSelector(state => state.auth)
 
   return (
     <Router>
@@ -75,9 +87,18 @@ function App() {
           <ProtectedRoute path="/password/update" component={UpdatePassword} exact />
 
           <ProtectedRoute path="/orders/me" component={ListOrders} exact />
-          <ProtectedRoute path="/order/:id" component={OrderDetails} />
+          <ProtectedRoute path="/order/:id" component={OrderDetails} exact />
+
         </div>
-        <Footer />
+        
+        <ProtectedRoute path="/dashboard" isAdmin={true} component={Dashboard} exact />
+        <ProtectedRoute path="/admin/products" isAdmin={true} component={ProductsList} exact />
+        <ProtectedRoute path="/admin/product" isAdmin={true} component={NewProduct} exact />
+        <ProtectedRoute path="/admin/product/:id" isAdmin={true} component={UpdateProduct} exact />
+        
+        {!loading && user.role !== 'admin' && (
+          <Footer />
+        )}
       </div>
     </Router>
   );
--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import { productsReducer, productDetailsReducer, newReviewReducer } from './reducers/productReducers';
+import { productsReducer, newProductReducer, productReducer, productDetailsReducer, newReviewReducer } from './reducers/productReducers';
 import { authReducer, userReducer, forgotPasswordReducer } from './reducers/userReducers';
 import { cartReducer } from './reducers/cartReducers';
 import { newOrderReducer, myOrdersReducer, orderDetailsReducer } from './reducers/orderReducers';
@@ -10,6 +10,8 @@ import { newOrderReducer, myOrdersReducer, orderDetailsReducer } from './reducer
 const reducer = combineReducers({
     products: productsReducer,
     productDetails: productDetailsReducer,
+    newProduct: newProductReducer,
+    product: productReducer,
     auth: authReducer,
     user: userReducer,
     forgotPassword: forgotPasswordReducer,
